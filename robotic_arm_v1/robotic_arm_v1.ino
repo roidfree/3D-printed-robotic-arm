c551ca4923b5fbdf5642b1c8f668c427a9ae4ec3
MaolinChen12 <ac2723@ic.ac.uk>
2024-03-21 10:05:11 +0000

    Update robotic_arm_v1.ino
    Final changes

--- robotic_arm_v1/robotic_arm_v1.ino
+++ robotic_arm_v1/robotic_arm_v1.ino
@@ -6,19 +6,18 @@ Servo myservo2;
 Servo myservo3;
 
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = A3;  // the number of the pushbutton pin
-const int claw = 8;      // the number of the electromagnet pin
+const int buttonPin = 3;  // the number of the pushbutton pin
+const int claw = A3;      // the number of the electromagnet pin
 const int elbowpot0 = A0;  //base mount motor analog pin used to connect the potentiometer
 const int elbowpot1 = A1; //pan motor 
 const int elbowpot2 = A2; 
-int buttonState = 0;  // variable for reading the pushbutton status
-bool magnet_state = false; // 
+bool buttonState = 0;  // variable for reading the pushbutton status
+bool magnet_state = 0; // 
 int elbowval0;             // variable to read the value from the analog pin
 int elbowval1;
-int elbowval2;
-
-// variables will change:
-//int buttonState = 0; variable for reading the pushbutton status
+int elbowval2; // variables will change:
+long nowmillis = 0;
+long lastmillis = 0;
 
 void setup() {
   // initialize the LED pin as an output:
@@ -29,31 +28,42 @@ void setup() {
   myservo0.attach(2);  // attaches the servo on pin 5 to the servo object for the grove
   myservo1.attach(4); //2nd
   myservo2.attach(6); 
+  myservo3.attach(7);
   digitalWrite(claw, magnet_state);
+  Serial.begin(9600);
 }
 
 
 void loop() {
   elbowval0 = analogRead(elbowpot0);        // reads the value of the potentiometer (value between 0 and 1023)
-  elbowval0 = map(elbowval0, 0, 1023, 10, 200);  // scale it for use with the servo (value between 0 and 180)
+  elbowval0 = map(elbowval0, 0, 1023, 60, 170);  // scale it for use with the servo (value between 0 and 180)
   myservo0.write(elbowval0);           // sets the servo position according to the scaled value
   elbowval1 = analogRead(elbowpot1);        // reads the value of the potentiometer (value between 0 and 1023)
-  elbowval1 = map(elbowval1, 0, 1023, 0, 180);  // scale it for use with the servo (value between 0 and 180)
+  elbowval1 = map(elbowval1, 0, 1023, 0, 80);  // scale it for use with the servo (value between 0 and 180)
   myservo1.write(elbowval1);           // sets the servo position according to the scaled value
+  elbowval1 = map(elbowval1, 0, 80, 80, 0);  // scale it for use with the servo (value between 0 and 180)
+  myservo2.write(elbowval1);           // sets the servo position according to the scaled value
   elbowval2 = analogRead(elbowpot2);        // reads the value of the potentiometer (value between 0 and 1023)
-  elbowval2 = map(elbowval2, 0, 1023, 0, 120);  // scale it for use with the servo (value between 0 and 180)
-  myservo2.write(elbowval2);           // sets the servo position according to the scaled value
+  elbowval2 = map(elbowval2, 0, 1023, 20, 100);  // scale it for use with the servo (value between 0 and 180)
+  myservo3.write(elbowval2);           // sets the servo position according to the scaled value
   buttonState = digitalRead(buttonPin);
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  buttonState = digitalRead(buttonPin);
+  // check if the push0button is pressed. If it is, the buttonState is HIGH:
+  //buttonState = digitalRead(buttonPin);
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
-    // turn LED on:
-    magnet_state != magnet_state;
-    digitalWrite(claw, magnet_state);
-  } 
-  
-  delay(50);                           // waits for the servo to get there
-}
+    nowmillis = millis();
+    if(nowmillis >= lastmillis + 500) {
 
-/* This is Aurora's changes */
+    
+    // turn LED on:
+    if (magnet_state == 1){magnet_state = 0;}
+    else if (magnet_state ==0){magnet_state =1;}
+    //magnet_state = 1;
+  //magnet_state = buttonState;
+        digitalWrite(claw, magnet_state);
+        Serial.println(magnet_state);
+        lastmillis = nowmillis;
+    }
+
+          }
+}
